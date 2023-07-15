72436b4c534b02be05a5a1b7b4d51bfd5536f7ac
abc <an@om.in>
2023-07-15 16:05:42 +0530

    before staring for front end

--- Mango.Services.CoupanAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CoupanAPI/Controllers/CouponAPIController.cs
@@ -62,7 +62,7 @@ namespace Mango.Services.CoupanAPI.Controllers
             }
             catch(Exception ex)
             {
-                _response.IsSucess = false;
+                _response.IsSuccess = false;
                 _response.Message = ex.Message;
             }
             return _response;
@@ -83,5 +83,61 @@ namespace Mango.Services.CoupanAPI.Controllers
             return _response;
         }
 
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+                _response.Result= _mapper.Map<CouponDto>(obj);
+
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try {
+           Coupon obj= _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex) {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon obj= _db.Coupons.First(u=>u.CouponId==id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();   
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
     }
 }
